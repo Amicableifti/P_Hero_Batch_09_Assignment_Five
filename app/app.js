92d2107b0d0d4d90dce8d224846397c4411ef245
ifti <amicableifti@gmail.com>
2024-02-18 22:57:45 +0000

    Add

--- app/app.js
+++ app/app.js
@@ -1,72 +1,74 @@
 // All JS
-
 // Get Class
 const allSeat = getByClassName("select-seat");
 
 // For Loop
 let totalseat = 8;
 let count = 0;
-let total = 0 ;
+let total = 0;
 let grandTotal = 0;
 for (const seat of allSeat) {
     seat.addEventListener("click", function (event) {
         // Count Increased
         count = count + 1;
-        total = total+550;
-        totalseat = totalseat - 1 ;
-        grandTotal = grandTotal+550 ;
-        
-       
+        total = total + 550;
+        totalseat = totalseat - 1;
+        grandTotal = grandTotal + 550;
+        var myArray = [];
+
         //get Clicked Seat number
         const seatNumber = event.target.innerText;
         const seatNumberhtml = event.target;
-        
+        // Push Array
+        myArray.push(seatNumber);
 
-        
         //Get Ticket Price
         const ticketPrice = getById("ticket-price");
-    
+
         // Div P1
         const seatList = getElById("seat-list");
 
         // CREATE ELEMENT NOW For Ticket NAME
         const p1 = document.createElement("span");
         p1.innerText = seatNumber;
-        
-       // CREATE ELEMENT NOW For Ticket NAME
-       const p2 = document.createElement("p");
-       p2.innerText = "Economoy";
+
+        // CREATE ELEMENT NOW For Ticket NAME
+        const p2 = document.createElement("p");
+        p2.innerText = "Economoy";
         // CREATE ELEMENT NOW For Ticket NAME
         const p3 = document.createElement("p");
         p3.innerText = ticketPrice;
 
-
-        // Append In P Tage Ticket Name
-        seatList.appendChild(p1);
-
-        // Append In P Tage Ticket Price
-        seatList.appendChild(p2);
-        // Append In P Tage Ticket Price
-        seatList.appendChild(p3);
-       
         //Validation check
+        console.log(count);
         if (count > 4) {
-             count = count - 1;
-             
+            alert("You can buy only four");
         }
-        console.log(seatNumberhtml);
-        seatNumberhtml.classList.add('!bg-green-500')
-        console.log(seatNumberhtml);
-      
-        // Set Seat Number 
-        setInnerText("seat-number-show",totalseat)
-        // 
-        setInnerText("seat-add", count);
-        // Set Totdal Price
-        setInnerText("total-price", total);
-         // Set Totdal Price
-        setInnerText("grand-total", grandTotal);
+        else {
+            // 
+            removeElemet("btn-sub");
+            // Append In P Tage Ticket Name
+            seatList.appendChild(p1);
+            // Append In P Tage Ticket Price
+            seatList.appendChild(p2);
+            // Append In P Tage Ticket Price
+            seatList.appendChild(p3);
+
+            seatNumberhtml.classList.add('!bg-green-500')
+            // Set Seat Number 
+            setInnerText("seat-number-show", totalseat)
+            // Count
+            setInnerText("seat-add", count);
+            // Set Totdal Price
+            setInnerText("total-price", total);
+            // Set Total Price
+            setInnerText("grand-total", grandTotal);
+            return 0;
+        }
+
 
     })
+
+
 }
 
--- app/utilities.js
+++ app/utilities.js
@@ -3,13 +3,13 @@
 // Get Element BY ID
 function getById(id) {
     const result = document.getElementById(id).innerText;
-    return result ;
+    return result;
 }
 // Get Element
 
 function getElById(id) {
     const result = document.getElementById(id);
-    return result ;
+    return result;
 }
 // Get Element BY Class
 function getByClassName(id) {
@@ -18,6 +18,66 @@ function getByClassName(id) {
 }
 
 // Set Innter Text
-function setInnerText (id,value) {
+function setInnerText(id, value) {
     document.getElementById(id).innerHTML = value;
+}
+
+// Discount Function
+function discount(c) {
+
+    let grandTotalFinal = 0;
+    var inputField = document.getElementById("discount-value");
+    var code = inputField.value;
+
+    let coupon = code;
+    if (coupon == "") {
+
+        setInnerText("grand-total", grandTotal);
+        return grandTotalFinal;
+    }
+    else if (coupon == "NEW15") {
+
+        let Temp = grandTotal * .15;
+        grandTotalFinal = grandTotal - Temp
+        hideElemet("btn-sub");
+        setInnerText("grand-total", grandTotalFinal);
+        return grandTotalFinal;
+    }
+
+    else if (coupon == "Couple 20") {
+
+        let Temp = grandTotal * .2;
+        grandTotalFinal = grandTotal - Temp
+        hideElemet("btn-sub");
+        setInnerText("grand-total", grandTotalFinal);
+        return grandTotalFinal;
+    }
+    else {
+
+        // const pp1 = document.createElement("span");
+        // pp1.innerText = "Invalid Coupon";
+        // const pp1list = getElById("btn-sub");
+        // pp1list.appendChild(pp1);
+
+        setInnerText("grand-total", grandTotal);
+        return grandTotal;
+    }
+
+    setInnerText("discount-value", "");
+    setInnerText("grand-total", grandTotalFinal);
+}
+
+function addBackgrounfColorByID(id){
+  const element = document.getElementById(id);
+  element.classList.add("!bg-green-500");
+}
+
+function hideElemet(element){
+    const subBtnhtml = document.getElementById(element);
+        subBtnhtml.classList.add('hidden')
+}
+
+function removeElemet(element){
+    const subBtnhtml = document.getElementById(element);
+        subBtnhtml.classList.remove('hidden')
 }
